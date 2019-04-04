c385862e8fe24e9b56d5a1a2883e9ec8ae2992dd
Miillky <vedran.milkovic25@gmail.com>
2019-04-04 20:16:21 +0200

    Završio Projekt

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Clicked {{ count }} times! Count is {{ parity }}.</p>
+    <button @click="increment" variant="success" >Increment</button>
+    <button @click="decrement" variant="danger">Decrement</button>
+    <button @click="incrementIfOdd" variant="info">Increment if Odd</button>
+    <button @click="incrementAsync" variant="warning">Increment Async</button>
+  </div>
+</template>
+
+<script>
+
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+
+export default {
+  name: 'Counter',
+  computed: {
+    ...mapState([
+      'count'
+    ]),
+    ...mapGetters([
+      'parity'
+    ])
+  },
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
